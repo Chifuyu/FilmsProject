0578080c3c40545b47a3139585dbbb146848f24b
Igor <harry.tallbelt@gmail.com>
2016-05-12 14:45:48 +0300

    database connection fixes
    Removed the database connection string and custom mapping and database
    initialization classes.

--- src/MovieRental/DAL/Context.cs
+++ src/MovieRental/DAL/Context.cs
@@ -5,36 +5,16 @@ using System.Web;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Configuration;
 using MovieRental.Models;
-using MovieRental.DAL.Mapping;
 
 namespace MovieRental.DAL
 {
     public class Context : DbContext
     {
-        public Context() : base("name=DbConnectionString")
-        {
-            Database.SetInitializer<Context>(new MoviesDbInitializer());
-        }
-
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Actor> Actors { get; set; }
         public DbSet<Client> Clients { get; set; }
         public DbSet<Director> Directors { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Order> Orders { get; set; }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            //Configure domain classes using Fluent API here
-            /*modelBuilder.HasDefaultSchema("dbo");
-
-            modelBuilder.Entity<Movie>().ToTable("Movies");
-
-            modelBuilder.Entity<Movie>().HasKey<int>(x => x.Id);
-            modelBuilder.Entity<Movie>().Property(x => x.Name).HasMaxLength(255).IsRequired();*/
-            //modelBuilder.Entity<Movie>().Map(delegate(EntityMappingConfiguration<Movie>))
-            modelBuilder.Configurations.Add(new MovieConfiguration());
-            base.OnModelCreating(modelBuilder);
-        }
     }
 }
